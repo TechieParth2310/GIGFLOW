57dbb178f38293d0ee9941ab1efbe93c77301870
Parth Kothawade <parthkothawade2310@gmail.com>
2026-01-13 23:10:58 +0530

    Add GET support to seed endpoint for browser access
    - Allow GET requests in addition to POST
    - Users can now open seed URL directly in browser
    - Fixes issue where browser default GET request was failing

--- server/src/routes/seedRoutes.js
+++ server/src/routes/seedRoutes.js
@@ -4,6 +4,8 @@ import { seedDatabase } from '../controllers/seedController.js';
 const router = express.Router();
 
 // Seed database endpoint (protected by secret key)
+// Support both GET and POST for easy browser access
+router.get('/', seedDatabase);
 router.post('/', seedDatabase);
 
 export default router;
